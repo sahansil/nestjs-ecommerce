a12f412bc30758749aa105e03bf504b5d200661b
sahansil <sahansilhumagain@gmail.com>
2025-11-07 11:45:57 +0545

    added bearer auth in swagger docs

--- src/main.ts
+++ src/main.ts
@@ -6,18 +6,23 @@ import { SwaggerModule , DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-app.enableCors();
+app.enableCors({
+    origin: 'http://127.0.0.1:5500',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+});
   const config = new DocumentBuilder()
     .setTitle('Ecommerce example')
     .setDescription('The cats API description')
     .setVersion('1.0')
     .addTag('cats')
+    .addBearerAuth()
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3006);
 }
 bootstrap();
 
